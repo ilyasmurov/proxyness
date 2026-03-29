80a53da4c061abb51161cb09c513fdb40054b99a
Ilya Smurov <ilya.smurov@gmail.com>
2026-03-29 20:01:31 +0700

    fix: correct loadFile path for production build

--- client/src/main/index.ts
+++ client/src/main/index.ts
@@ -21,7 +21,7 @@ function createWindow() {
   if (process.env.VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
+    mainWindow.loadFile(path.join(__dirname, "../../dist/index.html"));
   }
 
   // Minimize to tray instead of closing
